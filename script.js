4955f726dbc873849b3fbda4e09e95ab168ecc67
ikataria <17kataria@gmail.com>
2022-07-22 14:50:18 -0400

    second commit

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+let listNodes = document.getElementsByTagName("LI");
+
+for (let i = 0; i < listNodes.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("X");
+
+  span.className = "delete";
   span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+  listNodes[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
+// Cross(X) button will hide the clicked item from list.
+let deleteArr = document.getElementsByClassName("delete");
+for (let i = 0; i < deleteArr.length; i++) {
+  deleteArr[i].onclick = function() {
+    let div = this.parentElement;
     div.style.display = "none";
   }
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+// Whenever we click on task, it will be shown as checked
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ele) {
+  if (ele.target.tagName === 'LI') {
+    ele.target.classList.toggle('checked');
   }
 }, false);
 
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("inputValue").value;
-  var t = document.createTextNode(inputValue);
+// new
+function addTask() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("inputValue").value;
+  let t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You need to write something to add to the list!");
@@ -39,15 +41,15 @@ function newElement() {
   }
   document.getElementById("inputValue").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("X");
+  span.className = "delete";
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
+  for (i = 0; i < deleteArr.length; i++) {
+    deleteArr[i].onclick = function() {
+      let div = this.parentElement;
       div.style.display = "none";
     }
   }
