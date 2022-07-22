00863ecbd3569d2855bc1d6ca5601e3ec52f274b
ikataria <17kataria@gmail.com>
2022-07-22 18:35:24 -0400

    todo & contact list tested

---
+++ script/contactList.js
@@ -0,0 +1,50 @@
+document
+.querySelector('#btn-add-contact')
+.addEventListener('click', function(){
+    const inputName = document.querySelector('#name-input');
+    addContact(inputName.value);
+    inputName.value = "";
+})
+
+function addContact(fullName){
+    if(!fullName || !fullName.trim()) return;
+
+    const contactDiv = document.createElement('div');
+    contactDiv.className = "contact-entry";
+
+    contactDiv.addEventListener('dblclick', function(){
+        contactDiv.remove();
+    })
+
+    const nameInitialDiv = document.createElement('div');
+    nameInitialDiv.className = 'name-initial';
+    nameInitialDiv.textContent = fullName.charAt(0);
+    nameInitialDiv.style.backgroundColor = randomColor();
+    
+    const fullNameDiv = document.createElement('div');
+    fullNameDiv.className = 'full-name';
+    fullNameDiv.textContent = fullName;
+
+    // append in order
+    contactDiv.append(nameInitialDiv);
+    contactDiv.append(fullNameDiv);
+    document.querySelector('#contacts-list').append(contactDiv);
+}
+
+
+function randomColor(){
+    const colors =[
+        '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
+    ];
+
+    return colors[Math.floor(Math.random() * colors.length)];
+}
