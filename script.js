82e723d391f0310c88eabb01d30851404cae2b36
ikataria <17kataria@gmail.com>
2022-07-22 13:58:02 -0400

    style, idNames, script little bit changed

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ list.addEventListener('click', function(ev) {
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  var inputValue = document.getElementById("inputValue").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("You need to write something to add to the list!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("list").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.getElementById("inputValue").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
